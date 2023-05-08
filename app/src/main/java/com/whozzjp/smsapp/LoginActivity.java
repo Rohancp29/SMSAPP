a874f8c8781cc8846d350871ff589640f80e1e60
joshi-tejas07 <tejasj.technolitesolutions@gmail.com>
2023-05-08 17:34:47 +0530

    TJ

--- app/src/main/java/com/whozzjp/smsapp/LoginActivity.java
+++ app/src/main/java/com/whozzjp/smsapp/LoginActivity.java
@@ -1,14 +1,246 @@
 package com.whozzjp.smsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextInputEditText email, pass;
+    MaterialButton signBtn;
+    private FirebaseAuth auth;
+    MaterialTextView forget_password;
+    CheckInternet check=new CheckInternet();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.user_email);
+        pass = findViewById(R.id.user_pass);
+        signBtn = findViewById(R.id.signin);
+
+        // Initialize FirebaseApp
+        FirebaseApp.initializeApp(this);
+
+        // Initialize FirebaseAuth
+        auth = FirebaseAuth.getInstance();
+
+        //Check Permissions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (checkSelfPermission(android.Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+
+            }
+
+            else {
+                requestPermissions(new String[] {android.Manifest.permission.SEND_SMS, android.Manifest.permission.READ_CALL_LOG, android.Manifest.permission.READ_PHONE_STATE, Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS}, 1);
+            }
+        }
+        //Forget password
+        forget_password = findViewById(R.id.frg_password);
+        forget_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(LoginActivity.this, ForgetPasswordActivity.class));
+            }
+        });
+
+        signBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailET=email.getText().toString().trim();
+                String passET=pass.getText().toString().trim();
+
+                if (email.getText().toString().isEmpty()){
+                    email.setError("Enter email");
+                } else if (pass.getText().toString().isEmpty()) {
+                    pass.setError("Enter password");
+                }  else if (!email.getText().toString().isEmpty() && !pass.getText().toString().isEmpty()) {
+                    String txt_email = email.getText().toString();
+                    String txt_pass = pass.getText().toString();
+                    FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+                    DatabaseReference databaseReference=firebaseDatabase.getReference().child("admin");
+
+
+                    databaseReference.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            String emaildb=snapshot.child("email").getValue(String.class);
+
+                            String passworddb=snapshot.child("password").getValue(String.class);
+                            // Toast.makeText(MainActivity.this, ""+emaildb, Toast.LENGTH_SHORT).show();
+                            // Toast.makeText(MainActivity.this, ""+emailET, Toast.LENGTH_LONG).show();
+
+
+                            if(emailET.equals(emaildb) && passworddb.equals(passET)){
+
+                                // Set the login flag
+                                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(LoginActivity.this);
+                                SharedPreferences.Editor editor = preferences.edit();
+                                editor.putBoolean("loggedIn", true);
+                                editor.apply();
+
+                                startActivity(new Intent(LoginActivity.this, AdminActivity.class));
+
+                                finish();
+                                Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                    // Check the login flag when the app is reopened
+                    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(LoginActivity.this);
+                    boolean loggedIn = preferences.getBoolean("loggedIn", false);
+                    if(loggedIn) {
+
+                        startActivity(new Intent(getApplicationContext(), AdminActivity.class));
+                        finish();
+                    }
+
+                    //firebase login method
+                    loginuser(txt_email, txt_pass);
+                }else {
+                    Toast.makeText(LoginActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    //Firebase login
+
+    private void loginuser(String email, String password){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                    String id= auth.getUid();
+
+                    DatabaseReference root = FirebaseDatabase.getInstance().getReference().child("Users").child(id);
+                    root.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            boolean loggedIN= Boolean.TRUE.equals(snapshot.child("loggedIn").getValue(Boolean.class));
+                            if(!loggedIN){
+                                root.child("loggedIn").setValue(true);
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                //finish();
+                            }
+                            else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                builder.setTitle("Access Denied.");
+                                builder.setMessage("You have already login.");
+                                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        // Do something when the "OK" button is clicked
+                                        startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
+                                        dialog.dismiss();
+                                    }
+                                });
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+
+                }else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                    builder.setTitle("Account doesn't Exist.");
+                    builder.setMessage("Please signup yourself!!!");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Do something when the "OK" button is clicked
+                            startActivity(new Intent(getApplicationContext(),SignUpActivity.class));
+                            dialog.dismiss();
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION ) ;
+        registerReceiver(check, filter)  ;
+
+
+        super.onStart();
+       /* FirebaseUser currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }*/
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Create an alert dialog to ask the user if they want to exit
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirm");
+        builder.setMessage("Are you sure you want to exit?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    protected void onStop() {
+
+        unregisterReceiver(check);
+        super.onStop();
+        //  FirebaseAuth.getInstance().signOut();
     }
 }
--- app/src/main/java/com/whozzjp/smsapp/SplashScreen.java
+++ app/src/main/java/com/whozzjp/smsapp/SplashScreen.java
@@ -2,7 +2,12 @@ package com.whozzjp.smsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +15,41 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        // requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String versionName = packageInfo.versionName;
+
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(this, "e", Toast.LENGTH_SHORT).show();
+        }
+
+
+        Thread background = new Thread(){
+            public void run(){
+                try{
+                    sleep(3*1000);
+                    startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+
+                    finish();
+                }
+                catch (Exception e){}
+            }
+        };
+        background.start();
+
+
+
+
+
+
     }
+
 }
